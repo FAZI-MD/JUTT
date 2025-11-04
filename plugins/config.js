e53980abd71aea25522b2ff637deb1bbd1c82a94
FAZI-MD <fazijutt404@gmail.com>
2025-11-04 22:31:58 +0500

    Add files via upload

---
+++ plugins/config.js
@@ -0,0 +1,82 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "FAZI-MD~eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiT0ZGQlFraU93VHVwSXB6OG5EVlRmKzZrMU9zNEozYW5kSU1uOXZnNnAwRT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiSjB0Qi9hdEJ0VUc2VHVSRkthRmpBVVpqL25yK3hBMU9HK05Obm8vVFNXST0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiIwSzE2WThpd2RvVDB3U3h6K1BidkkxMmpQV1YrdWl6NGI2MXBQRnFOa0dRPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiI2VEZkN1k0cTVVZ05Oa2FTTTJWdkxITkxKQVBXUW42dEJlVllsUVUxa21ZPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjZIQlN0QmtpTzB5aHV1NjJRQ3FQcWFDZ2d3Y3V1aUt6U1VIalZyRXdsM3M9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjhzWE92aEkzOWU4WDhOOHZuVDczNzIxVjVybU94ck5NYzFnRWRtUlBBQ3c9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQUpuTUN2RnE2V01CaWY0Q3h5OGZCQWl6M1c4QnJVVk5oZGxxTWlDaDNrYz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQWpjNWppVjZPTGhWc1VxNmtOazdsek1RLytOS1d3TzBnVjR2cU1oYnZVRT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Ilk3K1FhSkZWY1JvNnNnTDMrcUNlcjdkSWg0cDc2MVQzOW5zekplUTVTWERrQjBHNm52a05EWFZLaTJySlVkN2YzVm5PdzVob0g0cDFIMC9uTWl3aENnPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTQ0LCJhZHZTZWNyZXRLZXkiOiJ1NWRSdWkvTWNvN3kwenF6eHJmcXRoS1I3Z2lDR0pRK2tIMTdjQlFaU3UwPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwicmVnaXN0ZXJlZCI6dHJ1ZSwicGFpcmluZ0NvZGUiOiJTNEtFQlFTNyIsIm1lIjp7ImlkIjoiOTIzMzQ1MzA5MTYwOjVAcy53aGF0c2FwcC5uZXQiLCJuYW1lIjoibm9vciIsImxpZCI6IjE0MjUxMzIyMjc4MDAxOjVAbGlkIn0sImFjY291bnQiOnsiZGV0YWlscyI6IkNJdWRzS29HRUs2U3BjY0dHQUVnQUNnQSIsImFjY291bnRTaWduYXR1cmVLZXkiOiJ4eklLenFOMlE0ZVNzSlVBeDFMLzZSaThGdzRwWXl4OHhQaUpNMVVKY1RFPSIsImFjY291bnRTaWduYXR1cmUiOiJYd0ZYSThGL2k1SUt0OC93MGtxelJNcHpOamdaTkVjRkdxWlBTcnVuK1p2OFVydWxPTzNYVHZsUWtxR3pyT0d2c21paVFjMElLTHpVZ3pxR3lRTUNCUT09IiwiZGV2aWNlU2lnbmF0dXJlIjoiMGNsNGcxZG5nMXo3L3haUHpaN0xKSVhLQlZIYTR6SWVtOS90NnNTdjFDSDV5dVpPemxzSCtDTmlwa0YvejZaNS9rTnZndGZKd3lGMnB6S0kzc0tLQ2c9PSJ9LCJzaWduYWxJZGVudGl0aWVzIjpbeyJpZGVudGlmaWVyIjp7Im5hbWUiOiI5MjMzNDUzMDkxNjA6NUBzLndoYXRzYXBwLm5ldCIsImRldmljZUlkIjowfSwiaWRlbnRpZmllcktleSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJjY3lDczZqZGtPSGtyQ1ZBTWRTLytrWXZCY09LV01zZk1UNGlUTlZDWEV4In19XSwicGxhdGZvcm0iOiJzbWJhIiwicm91dGluZ0luZm8iOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJDQklJQlE9PSJ9LCJsYXN0QWNjb3VudFN5bmNUaW1lc3RhbXAiOjE3NjAxMTkwOTAsImxhc3RQcm9wSGFzaCI6IjFLNGhINCIsIm15QXBwU3RhdGVLZXlJZCI6IkFBQUFBRFA0In0=",
+// add your Session Id 
+AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
+// make true or false status auto seen
+AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
+// make true if you want auto reply on status 
+AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
+// make true if you want auto reply on status 
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY FAZI-MD 🤍*",
+// set the auto reply massage on status reply  
+ANTI_DELETE: process.env.ANTI_DELETE || "true",
+// set true false for anti delete     
+ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "inbox", 
+// change it to 'same' if you want to resend deleted message in same chat     
+WELCOME: process.env.WELCOME || "true",
+// true if want welcome and goodbye msg in groups    
+ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
+// make true to know who dismiss or promoted a member in group
+ANTI_LINK: process.env.ANTI_LINK || "true",
+// make anti link true,false for groups 
+MENTION_REPLY: process.env.MENTION_REPLY || "false",
+// make true if want auto voice reply if someone menetion you 
+MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/f7y3rn.jpg",
+// add custom menu and mention reply image url
+PREFIX: process.env.PREFIX || ".",
+// add your prifix for bot   
+BOT_NAME: process.env.BOT_NAME || "FAZI-MD",
+// add bot namw here for menu
+STICKER_NAME: process.env.STICKER_NAME || "FAZI-MD",
+// type sticker pack name 
+CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
+// make this true for custum emoji react    
+CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
+// chose custom react emojis by yourself 
+DELETE_LINKS: process.env.DELETE_LINKS || "false",
+// automatic delete links witho remove member 
+OWNER_NUMBER: process.env.OWNER_NUMBER || "923266105873",
+// add your bot owner number
+OWNER_NAME: process.env.OWNER_NAME || "*Fazi jutt*",
+// add bot owner name
+DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ Fazi jutt ❣️*",
+// add bot owner name    
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/f7y3rn.jpg",
+// add img for alive msg
+LIVE_MSG: process.env.LIVE_MSG || "> Zinda Hun Yar ⚡",
+// add alive msg here 
+READ_MESSAGE: process.env.READ_MESSAGE || "false",
+// Turn true or false for automatic read msgs
+AUTO_REACT: process.env.AUTO_REACT || "false",
+// make this true or false for auto react on all msgs
+ANTI_BAD: process.env.ANTI_BAD || "false",
+// false or true for anti bad words  
+MODE: process.env.MODE || "public",
+// make bot public-private-inbox-group 
+ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "false",
+// make anti link true,false for groups 
+AUTO_STICKER: process.env.AUTO_STICKER || "false",
+// make true for automatic stickers 
+AUTO_REPLY: process.env.AUTO_REPLY || "false",
+// make true or false automatic text reply 
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+// maks true for always online 
+PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
+// make false if want private mod
+AUTO_TYPING: process.env.AUTO_TYPING || "false",
+// true for automatic show typing   
+READ_CMD: process.env.READ_CMD || "false",
+// true if want mark commands as read 
+DEV: process.env.DEV || "923266105873",
+//replace with your whatsapp number        
+ANTI_VV: process.env.ANTI_VV || "true",
+// true for anti once view 
+AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
+// make it true for auto recoding 
+};
